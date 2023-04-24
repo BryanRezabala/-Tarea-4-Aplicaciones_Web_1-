efe4a18a770a97c7713c61322b33f79199576f72
BryanRezabala <rbryandavid@gmail.com>
2023-04-23 21:46:55 -0500

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,52 @@
+//Primera iteración(())
+function SumaValores(){
+    let valor1 = prompt("Ingrese el primer valor:");
+    let valor2 = prompt("Ingrese el segundo valor:");
+    let num1 = parseInt(valor1);
+    let num2 = parseInt(valor2);
+    let resultado = num1 + num2;
+    alert(`El resultado de la suma es: ${resultado}`);
+    
+}
+
+//Segunda iteración(())
+function RestaValores(){
+    let valor1 = prompt("Ingrese el primer valor:");
+    let valor2 = prompt("Ingrese el segundo valor:");
+    let num1 = parseInt(valor1);
+    let num2 = parseInt(valor2);
+    let resultado = num1 - num2;
+    alert(`El resultado de la resta es: ${resultado}`);
+}
+
+//Tercera iteración(())
+function MultiplicaciónValores(){
+let valor1 = prompt("Ingrese el primer valor:");
+let valor2 = prompt("Ingrese el segundo valor:");
+let num1 = parseInt(valor1);
+let num2 = parseInt(valor2);
+let resultado = num1 * num2;
+alert(`El resultado de la multiplicación es: ${resultado}`);
+}
+
+//Cuarta iteración(())
+function DivisiónValores(){
+    let valor1 = prompt("Ingrese el primer valor:");
+    let valor2 = prompt("Ingrese el segundo valor:");
+    let num1 = parseInt(valor1);
+    let num2 = parseInt(valor2);
+    let resultado = num1 / num2;
+    alert(`El resultado de la división es: ${resultado}`);
+    
+    }
+
+//Quinta iteración(())
+function MODValores(){
+    let valor1 = prompt("Ingrese el primer valor:");
+    let valor2 = prompt("Ingrese el segundo valor:");
+    let num1 = parseInt(valor1);
+    let num2 = parseInt(valor2);
+    let resultado = num1 % num2;
+    alert(`El resultado del módulo es: ${resultado}`);
+    
+    }
